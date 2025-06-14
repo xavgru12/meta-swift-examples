e95ecc499a2e06a55fb79e7651c65d852773b38e
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-06-14 14:44:56 +0200

    cleanup

--- env.sh
+++ env.sh
@@ -4,19 +4,10 @@
 
 # Docker
 
-DOCKER_IMAGE_TAG="nxp-yocto-evaluation"
+DOCKER_IMAGE_TAG="meta-swift-project"
 DOCKER_WORKDIR="$PWD"
 
 # Yocto
 
-IMX_RELEASE="imx-6.6.52-2.2.0"
-
 YOCTO_DIR="$PWD"
 
-MACHINE="imx8mnevk"
-DISTRO="fsl-imx-xwayland"
-IMAGES="imx-image-core"
-
-REMOTE="https://github.com/nxp-imx/imx-manifest"
-BRANCH="imx-linux-scarthgap"
-MANIFEST=${IMX_RELEASE}".xml"
