bd4aa98ae9c516e68dcc178062898ab40b6e0e2b
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-06-24 23:13:15 +0200

    interact on

--- execute.sh
+++ execute.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/expect -f
 
-#spawn bash -c "cd poky && source oe-init-build-env && runqemu qemuarm nographic"
-#expect "*login:*"
-#send "root"
-#interact
-
 spawn bash -c "cd poky && source oe-init-build-env && runqemu qemuarm nographic"
 
 # Wait for login prompt exactly
@@ -18,5 +13,5 @@ expect -re {root@.*# }
 send "hello-world\r"
 
 # Optional: interact to keep session open
-#interact
+interact
 
