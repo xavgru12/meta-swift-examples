bd1643db274a67ac026aff094b417af44bde6d23
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-06-25 22:32:17 +0200

    server run

---
+++ server-run-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+. ./env.sh
+
+if [[ $PWD != $HOME* && $(whoami) != "root" ]]; then
+    echo "Error: Current directory is outside $HOME"
+    exit 1
+fi
+
+# run the docker image
+docker run -i --rm \
+  --volume ${HOME}:${HOME} \
+  --device /dev/net/tun \
+  --cap-add=NET_ADMIN \
+    "${DOCKER_IMAGE_TAG}" \
+    $1
